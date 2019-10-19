6c70348d568623ce02d4df249ea83e2b2e9510eb
sudipto <udipto.raj23@gmail.com>
2019-10-19 20:37:51 +0530

    2nd commit

--- app/src/main/java/com/globussoft/weather/MainActivity.java
+++ app/src/main/java/com/globussoft/weather/MainActivity.java
@@ -3,8 +3,10 @@ package com.globussoft.weather;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,11 +19,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import static com.globussoft.weather.StartActivity.LAT;
+import static com.globussoft.weather.StartActivity.LON;
+
 public class MainActivity extends AppCompatActivity {
+    String response=null;
     String CITY = "delhi,in";
     static String API = "6c4365883abfda0b793574f7c88863af";
-    public static String LAT=null,LON=null;
-
     TextView addressTxt, updated_atTxt, statusTxt, tempTxt, temp_minTxt, temp_maxTxt, sunriseTxt,
             sunsetTxt, windTxt, pressureTxt, humidityTxt;
 
@@ -52,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         protected String doInBackground(String... args) {
-            String response;
             if(LAT!=null&&LON!=null) {
                 response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?lat=" + LAT + "&lon=" + LON + "&units=metric&appid=" + API);
             }
@@ -65,42 +68,50 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             try {
-                JSONObject jsonObj = new JSONObject(result);
-                JSONObject main = jsonObj.getJSONObject("main");
-                JSONObject sys = jsonObj.getJSONObject("sys");
-                JSONObject wind = jsonObj.getJSONObject("wind");
-                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+                if(response!=null)
+                {
+                    JSONObject jsonObj = new JSONObject(result);
+                    JSONObject main = jsonObj.getJSONObject("main");
+                    JSONObject sys = jsonObj.getJSONObject("sys");
+                    JSONObject wind = jsonObj.getJSONObject("wind");
+                    JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
 
-                Long updatedAt = jsonObj.getLong("dt");
-                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
-                String temp = main.getString("temp") + "°C";
-                String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
-                String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
-                String pressure = main.getString("pressure");
-                String humidity = main.getString("humidity");
+                    Long updatedAt = jsonObj.getLong("dt");
+                    String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+                    String temp = main.getString("temp") + "°C";
+                    String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
+                    String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
+                    String pressure = main.getString("pressure");
+                    String humidity = main.getString("humidity");
 
-                Long sunrise = sys.getLong("sunrise");
-                Long sunset = sys.getLong("sunset");
-                String windSpeed = wind.getString("speed");
-                String weatherDescription = weather.getString("description");
+                    Long sunrise = sys.getLong("sunrise");
+                    Long sunset = sys.getLong("sunset");
+                    String windSpeed = wind.getString("speed");
+                    String weatherDescription = weather.getString("description");
 
-                String address = jsonObj.getString("name") + ", " + sys.getString("country");
-                addressTxt.setText(address);
-                updated_atTxt.setText(updatedAtText);
-                statusTxt.setText(weatherDescription.toUpperCase());
-                tempTxt.setText(temp);
-                temp_minTxt.setText(tempMin);
-                temp_maxTxt.setText(tempMax);
-                sunriseTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunrise * 1000)));
-                sunsetTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunset * 1000)));
-                windTxt.setText(windSpeed);
-                pressureTxt.setText(pressure);
-                humidityTxt.setText(humidity);
-                findViewById(R.id.loader).setVisibility(View.GONE);
-                findViewById(R.id.mainContainer).setVisibility(View.VISIBLE);
+                    String address = jsonObj.getString("name") + ", " + sys.getString("country");
+                    addressTxt.setText(address);
+                    updated_atTxt.setText(updatedAtText);
+                    statusTxt.setText(weatherDescription.toUpperCase());
+                    tempTxt.setText(temp);
+                    temp_minTxt.setText(tempMin);
+                    temp_maxTxt.setText(tempMax);
+                    sunriseTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunrise * 1000)));
+                    sunsetTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunset * 1000)));
+                    windTxt.setText(windSpeed);
+                    pressureTxt.setText(pressure);
+                    humidityTxt.setText(humidity);
+                    findViewById(R.id.loader).setVisibility(View.GONE);
+                    findViewById(R.id.mainContainer).setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"No Weather Report right now\n showing default city weather",Toast.LENGTH_SHORT).show();
+                    Log.d("No weather report","Showing default location");
+                }
 
 
-            } catch (JSONException e) {
+            } catch (Exception e) {
                 findViewById(R.id.loader).setVisibility(View.GONE);
                 findViewById(R.id.errorText).setVisibility(View.VISIBLE);
             }
--- app/src/main/java/com/globussoft/weather/StartActivity.java
+++ app/src/main/java/com/globussoft/weather/StartActivity.java
@@ -2,7 +2,6 @@ package com.globussoft.weather;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -11,19 +10,22 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.location.Location;
-
 import android.location.LocationManager;
-import android.net.Uri;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.androdocs.httprequest.HttpRequest;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
@@ -35,26 +37,30 @@ import com.google.android.gms.location.LocationServices;
 import org.json.JSONObject;
 
 import static com.globussoft.weather.MainActivity.API;
-import static com.globussoft.weather.StartActivity.LAT;
-import static com.globussoft.weather.StartActivity.LON;
-import static com.globussoft.weather.StartActivity.MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
 
-public class LocationActivity extends AppCompatActivity implements LocationListener,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
-    private long UPDATE_INTERVAL = 2 * 1000;
-    private long FASTEST_INTERVAL = 2000;
-    TextView lattitude,longitude,address;
+public class StartActivity extends AppCompatActivity implements LocationListener,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
+    private static int Splash_time_out=4000;
+    TextView textView;
+    ProgressBar progressBar;
+    Button button;
+    boolean available=false;
+    boolean haveConnectedWifi = false;
+    boolean haveConnectedMobile = false;
+    public static String LAT=null,LON=null;
     GoogleApiClient gac;
     LocationRequest locationRequest;
+    static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private long UPDATE_INTERVAL = 2 * 1000;
+    private long FASTEST_INTERVAL = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_location);
-        lattitude=findViewById(R.id.lattitude);
-        longitude=findViewById(R.id.longitude);
-        address=findViewById(R.id.address);
-        if(!isLocationEnabled()) {
-            showAlert();
-        }
+        setContentView(R.layout.activity_start);
+        textView=findViewById(R.id.checktv);
+        progressBar=findViewById(R.id.progress);
+        button=findViewById(R.id.out);
+        isGooglePlayServicesAvailable();
+            new InternetTask().execute();
             locationRequest = new LocationRequest();
             locationRequest.setInterval(UPDATE_INTERVAL);
             locationRequest.setFastestInterval(FASTEST_INTERVAL);
@@ -65,7 +71,6 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
                     .addApi(LocationServices.API)
                     .build();
     }
-
     @Override
     protected void onStart() {
         gac.connect();
@@ -77,21 +82,13 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
         gac.disconnect();
         super.onStop();
     }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        if (location != null) {
-            updateUI(location);
-        }
-    }
-
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(LocationActivity.this,
+            ActivityCompat.requestPermissions(StartActivity.this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
 
@@ -100,7 +97,6 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
         Location ll = LocationServices.FusedLocationApi.getLastLocation(gac);
         LocationServices.FusedLocationApi.requestLocationUpdates(gac, locationRequest, this);
     }
-
     @Override
     public void onRequestPermissionsResult(
             int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -108,47 +104,60 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(LocationActivity.this, "Permission was granted!", Toast.LENGTH_LONG).show();
+                    Toast.makeText(StartActivity.this, "Permission was granted!", Toast.LENGTH_LONG).show();
 
                     try{
                         LocationServices.FusedLocationApi.requestLocationUpdates(gac, locationRequest, this);
                     } catch (SecurityException e) {
-                        Toast.makeText(LocationActivity.this, "SecurityException:\n" + e.toString(),
+                        Toast.makeText(StartActivity.this, "SecurityException:\n" + e.toString(),
                                 Toast.LENGTH_LONG).show();
                     }
                 } else {
-                    Toast.makeText(LocationActivity.this, "Permission denied!", Toast.LENGTH_LONG).show();
+                    Toast.makeText(StartActivity.this, "Permission denied!", Toast.LENGTH_LONG).show();
                 }
                 return;
             }
         }
     }
-
     @Override
     public void onConnectionSuspended(int i) {}
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Toast.makeText(LocationActivity.this, "onConnectionFailed: \n" + connectionResult.toString(),
+        Toast.makeText( StartActivity.this, "onConnectionFailed: \n" + connectionResult.toString(),
                 Toast.LENGTH_LONG).show();
         Log.d("DDD", connectionResult.toString());
     }
 
-    private void updateUI(Location loc) {
-        LAT=(Double.toString(loc.getLatitude()));
-        LON=(Double.toString(loc.getLongitude()));
-        lattitude.setText(LAT);
-        longitude.setText(LON);
-        new LocationTask().execute();
-    }
-
     private boolean isLocationEnabled()
     {
+        Log.d("Location","enabled? checked");
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+    }
+    private boolean isGooglePlayServicesAvailable()
+    {
+        final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode))
+            {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            }
+            else
+            {
+                Log.d("gps", "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        Log.d("gps", "This device is supported.");
+        return true;
     }
     private void showAlert() {
-        new android.app.AlertDialog.Builder(this).
+        new android.app.AlertDialog.Builder(StartActivity.this).
                 setTitle("Enable Location")
                 .setIcon(R.drawable.ic_dialog)
                 .setMessage("To Continue \nGo to settings and Enable Location")
@@ -163,37 +172,78 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-                        Toast.makeText(getApplicationContext(),"Unable to Fetch Location",Toast.LENGTH_SHORT).show();
+                        textView.setText("Location Disabled");
+                        progressBar.setVisibility(View.INVISIBLE);
+                        button.setVisibility(View.VISIBLE);
                     }
                 }).show();
     }
-    public void find(View view)
-    {
-        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
-                Uri.parse("geo:0,0?q="+LAT+","+LON+" (" + "you" + ")"));
-        startActivity(intent);
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        LAT=(Double.toString(location.getLatitude()));
+        LON=(Double.toString(location.getLongitude()));
+        if(available)
+        new LocationTask().execute();
     }
-    public void showData(View view)
+
+    class InternetTask extends AsyncTask<Void,Void,Void>
     {
-        Intent intent=new Intent(LocationActivity.this,MainActivity.class);
-        startActivity(intent);
-        finishAffinity();
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+            for (NetworkInfo ni : netInfo) {
+                if (ni.getTypeName().equalsIgnoreCase("WIFI")) {
+                    if (ni.isConnected()) {
+                        Log.d("sssssssss","wifi is connected");
+                        haveConnectedWifi = true;
+                    }
+                }
+                if (ni.getTypeName().equalsIgnoreCase("MOBILE")) {
+                    if (ni.isConnected()) {
+                        Log.d("sssssssss","Mobile is connected");
+                        haveConnectedMobile = true;
+                    }
+                }
+                if(haveConnectedWifi || haveConnectedMobile){
+                    available=true;
+                }
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid)
+        {
+            super.onPostExecute(aVoid);
+            if(!available) {
+                textView.setText("No Internet Connection");
+                progressBar.setVisibility(View.INVISIBLE);
+                button.setVisibility(View.VISIBLE);
+            }
+            else if (!isLocationEnabled()) {
+                showAlert();
+            }
+            else{
+                onStart();
+            }
+
+        }
     }
     class LocationTask extends AsyncTask<Void,Void,Void>
     {
         String add;
         @Override
         protected Void doInBackground(Void... voids) {
-            String response=HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?lat=" + LAT + "&lon=" + LON + "&units=metric&appid=" + API);
-            add="could not get current location";
+            String response= HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?lat=" + LAT + "&lon=" + LON + "&units=metric&appid=" + API);
+            add="delhi,in";
             try {
                 if (response != null) {
                     JSONObject jsonObj = new JSONObject(response);
                     JSONObject sys = jsonObj.getJSONObject("sys");
                     add = jsonObj.getString("name") + ", " + sys.getString("country");
-                } else {
-                    Toast.makeText(getApplicationContext(), "Currently Location data not found", Toast.LENGTH_SHORT).show();
-                    Log.d("Location not found", "Showing default location");
                 }
             }
             catch(Exception e)
@@ -206,8 +256,14 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
 
         @Override
         protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            address.setText(add);
+                    Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
         }
     }
+    public void out(View view)
+    {
+        finish();
+    }
+
 }
